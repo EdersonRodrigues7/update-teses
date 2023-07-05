f2f29c4da6fa88a7bb2592c69af40930d251ab13
Ederson Rodrigues <ederson@trilhante.com.br>
2023-07-05 11:34:03 -0300

    add environment variables

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,7 +1,8 @@
 const { join } = require('path');
+const { env } = require('process');
 /**
  * @type {import("puppeteer").PuppeteerConfiguration}
  */
 module.exports = {
-    cacheDirectory: join(__dirname, 'node_modules', '.cache', 'puppeteer')
+    cacheDirectory: join(__dirname, env.PUPPETEER_CACHE_DIR)
 };
