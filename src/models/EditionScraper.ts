4a5de8b63605ed9e28a8c49f6937ba8f368f13af
Ederson Rodrigues <ederson@trilhante.com.br>
2023-07-07 09:38:32 -0300

    sandbox issue

--- src/models/EditionScraper.ts
+++ src/models/EditionScraper.ts
@@ -70,7 +70,10 @@ class EditionScraper {
         //     headless: chromium.headless,
         //     executablePath: await chromium.executablePath("/browsers/chromium")
         // });
-        const browser = await puppeteer.launch({ headless: "new" })
+        const browser = await puppeteer.launch({
+            headless: "new",
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        })
         const page = await browser.newPage();
         await page.setUserAgent(
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
